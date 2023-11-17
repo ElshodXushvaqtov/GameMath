5539ae4fae9212cc37489883a7146ca02d363d9f
Msi <elshodxushvaqtov07@gmail.com>
2023-11-17 19:08:55 +0500

    hassaqwssa

--- app/src/main/java/com/example/mathgame/OperatorScreen.kt
+++ app/src/main/java/com/example/mathgame/OperatorScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -15,9 +14,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.mathgame.navigation.NavGraph
 
@@ -32,7 +31,9 @@ fun OperatorScreen(navController: NavHostController) {
     ) {
 
         Button(
-            onClick = {},
+            onClick = {
+                navController.navigate("new_game_screen")
+            },
             Modifier
                 .width(70.dp)
                 .height(70.dp),
